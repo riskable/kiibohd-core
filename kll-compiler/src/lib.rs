3339da07c7a54014bf4c1b1fcb340adfe8a75f5e
Smasher816 <Smasher816@gmail.com>
2021-01-24 14:14:34 -0800

    Parse the rest of the triggers and animation internals
    TODO: This needs to be cleaned up badly

--- kll-compiler/src/lib.rs
+++ kll-compiler/src/lib.rs
@@ -8,66 +8,194 @@ type PestError = pest::error::Error<Rule>;
 #[grammar = "kll.pest"]
 pub struct KLLParser;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Variable<'a> {
     List(Vec<&'a str>),
     String(&'a str),
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Position {
-    x: usize,
-    y: usize,
-    z: usize,
-    rx: usize,
-    ry: usize,
-    rz: usize,
+    x: usize,  // mm
+    y: usize,  // mm
+    z: usize,  // mm
+    rx: usize, // deg
+    ry: usize, // deg
+    rz: usize, // deg
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct PixelDef {
     channels: Vec<(usize, usize)>,
-    scancode: Option<String>,
+    scancode: Option<usize>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Animation<'a> {
     modifiers: Vec<&'a str>,
-    frames: Vec<&'a str>,
+    frames: Vec<Pixel>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone)]
 pub struct Capability<'a> {
     function: &'a str,
     args: Vec<&'a str>,
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum KeyState {
+    Press(usize),   // (P:time)
+    Hold(usize),    // (H:time)
+    Release(usize), // (R:time)
+    Off,            // (O).  Not available for output
+    UniquePress,    // (UP). Not available for output
+    UniqueRelease,  // (UR). Not available for output
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum KeyGroup<'a> {
+    Single(Key<'a>),
+    Sequence(Vec<KeyGroup<'a>>),
+    Combination(Vec<KeyGroup<'a>>),
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct KeyTrigger<'a> {
+    keys: KeyGroup<'a>,
+    press_state: Option<KeyState>,
+    analog_state: Option<usize>, // percent (0-100)
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum GenericState {
+    Activate,   // (A)
+    On,         // (On)
+    Deactivate, // (D)
+    Off,        // (Off)
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct IndicatorTrigger {
+    indicator: usize,
+    state: Option<GenericState>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum LayerMode {
+    Normal,
+    Shift,
+    Latch,
+    Lock,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct LayerTrigger {
+    layer: usize,
+    mode: LayerMode,
+    state: Option<GenericState>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct GenericTrigger {
+    bank: usize,
+    index: usize,
+    param: Option<usize>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum Trigger<'a> {
-    Key(usize),
+    Key(KeyTrigger<'a>),
     Other(&'a str),
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
-pub enum Action<'a> {
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum Key<'a> {
+    Scancode(usize),
     Usb(&'a str),
     Consumer(&'a str),
     System(&'a str),
     Other(&'a str),
+    None,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum Action<'a> {
+    Output(KeyTrigger<'a>),
+    Layer(LayerTrigger),
+    Animation(AnimationAction<'a>),
+    Pixel(Pixel),
+    PixelLayer(Pixel),
+    Capability((Capability<'a>, KeyState)),
+    Other(&'a str),
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum TriggerVarient {
+    Replace,     // :
+    SoftReplace, // ::
+    Add,         // :+
+    Remove,      // :-
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum PixelAddr {
+    Absolute(usize),
+    RelativeInt(usize),
+    RelativePercent(usize),
+}
+
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone)]
+pub struct PixelRange {
+    index: Option<PixelAddr>,
+    row: Option<PixelAddr>,
+    col: Option<PixelAddr>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct AnimationTrigger<'a> {
+    name: &'a str,
+    state: GenericState,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct AnimationAction<'a> {
+    name: &'a str,
+    args: Vec<&'a str>,
+}
+
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone)]
+pub struct Pixel {
+    range: PixelRange,
+    channel_values: Vec<PixelColor>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum PixelColor {
+    Rgb(usize),
+    Relative(isize),
+}
+
+#[derive(Debug, Clone)]
+pub struct Mapping<'a> {
+    trigger: Trigger<'a>,
+    mode: TriggerVarient,
+    isolate: bool,
+    result: Action<'a>,
+}
+
+#[derive(Debug, Default, Clone)]
 pub struct KllState<'a> {
     defines: HashMap<&'a str, &'a str>,
     variables: HashMap<&'a str, Variable<'a>>,
     capabilities: HashMap<&'a str, Capability<'a>>,
-    keymap: HashMap<Trigger<'a>, Action<'a>>,
+    keymap: Vec<Mapping<'a>>,
     positions: HashMap<usize, Position>,
-    pixelmap: HashMap<&'a str, PixelDef>,
+    pixelmap: HashMap<usize, PixelDef>,
     animations: HashMap<&'a str, Animation<'a>>,
 }
 
 fn parse_int(s: &str) -> usize {
+    //dbg!(s);
     if s.starts_with("0x") {
         usize::from_str_radix(s.trim_start_matches("0x"), 16).unwrap()
     } else {
@@ -141,28 +269,104 @@ impl KllState<'_> {
                 Rule::mapping => {
                     let mut parts = line.into_inner();
                     let lhs = parts.next().unwrap();
-                    let mode = parts.next().unwrap().as_str();
+                    let assignment = parts.next().unwrap().as_str();
                     let rhs = parts.next().unwrap();
+
+                    let (isolate, mode) = match assignment {
+                        ":" => (false, TriggerVarient::Replace),
+                        "::" => (false, TriggerVarient::SoftReplace),
+                        ":+" => (false, TriggerVarient::Add),
+                        ":-" => (false, TriggerVarient::Remove),
+                        "i:" => (true, TriggerVarient::Replace),
+                        "i::" => (true, TriggerVarient::SoftReplace),
+                        "i:+" => (true, TriggerVarient::Add),
+                        "i:-" => (true, TriggerVarient::Remove),
+                        _ => unreachable!(),
+                    };
 
                     let text = lhs.as_str();
                     let trigger = match lhs.into_inner().next().unwrap().as_rule() {
                         Rule::key_trigger => {
-                            let key = text.strip_prefix("S").unwrap();
-                            Trigger::Key(parse_int(key))
+                            let scancode = text.strip_prefix("S").unwrap();
+                            let key = Key::Scancode(parse_int(scancode));
+                            let trigger = KeyTrigger {
+                                keys: KeyGroup::Single(key),
+                                press_state: None,
+                                analog_state: None,
+                            };
+                            Trigger::Key(trigger)
                         }
                         _ => unimplemented!(),
                     };
 
                     let text = rhs.as_str();
-                    let result = match rhs.into_inner().next().unwrap().as_rule() {
-                        Rule::usbcode => Action::Usb(text.strip_prefix("U").unwrap()),
-                        Rule::consumer => Action::Consumer(text.strip_prefix("CON").unwrap()),
-                        Rule::system => Action::System(text.strip_prefix("SYS").unwrap()),
-                        Rule::color => Action::Other(text),
+                    let rhs = rhs.into_inner().next().unwrap();
+                    let result = match dbg!(rhs.as_rule()) {
+                        Rule::usbcode => {
+                            let usbcode = text.strip_prefix("U").unwrap();
+                            let key = Key::Usb(usbcode);
+                            let trigger = KeyTrigger {
+                                keys: KeyGroup::Single(key),
+                                press_state: None,
+                                analog_state: None,
+                            };
+                            Action::Output(trigger)
+                        }
+                        Rule::consumer => {
+                            let code = text.strip_prefix("CON").unwrap();
+                            let key = Key::Consumer(code);
+                            let trigger = KeyTrigger {
+                                keys: KeyGroup::Single(key),
+                                press_state: None,
+                                analog_state: None,
+                            };
+                            Action::Output(trigger)
+                        }
+                        Rule::system => {
+                            let code = text.strip_prefix("Sys").unwrap();
+                            let key = Key::System(code);
+                            let trigger = KeyTrigger {
+                                keys: KeyGroup::Single(key),
+                                press_state: None,
+                                analog_state: None,
+                            };
+                            Action::Output(trigger)
+                        }
+                        Rule::color => {
+                            let mut parts = rhs.into_inner();
+                            let index = parts.next().unwrap().as_str();
+                            let channels = parts.next().unwrap();
+
+                            let mut values = vec![];
+                            for c in channels.into_inner() {
+                                let color = c.as_str();
+                                let value = match &color[0..1] {
+                                    "+" | "-" => {
+                                        PixelColor::Relative(color.parse::<isize>().unwrap())
+                                    }
+                                    _ => PixelColor::Rgb(parse_int(color)),
+                                };
+                                values.push(value);
+                            }
+                            let pixel = Pixel {
+                                range: PixelRange {
+                                    index: Some(PixelAddr::Absolute(parse_int(index))),
+                                    ..Default::default()
+                                },
+                                channel_values: values,
+                            };
+
+                            Action::Pixel(pixel)
+                        }
                         _ => unimplemented!(),
                     };
 
-                    kll.keymap.insert(trigger, result);
+                    kll.keymap.push(Mapping {
+                        trigger,
+                        mode,
+                        isolate,
+                        result,
+                    });
                 }
                 Rule::position => {
                     let mut parts = line.into_inner();
@@ -191,9 +395,15 @@ impl KllState<'_> {
                     let mut lhs = parts.next().unwrap().into_inner();
                     let scancode = parts.next().unwrap().as_str();
 
-                    let name = lhs.next().unwrap().as_str();
+                    let index = parse_int(lhs.next().unwrap().as_str());
                     let mut pixel = PixelDef {
-                        scancode: Some(scancode.to_string()),
+                        scancode: Some(
+                            scancode
+                                .strip_prefix("S")
+                                .unwrap()
+                                .parse::<usize>()
+                                .unwrap(),
+                        ),
                         ..Default::default()
                     };
 
@@ -206,7 +416,7 @@ impl KllState<'_> {
                     }
 
                     //println!("PIXEL '{}' -> '{}'", name, value);
-                    kll.pixelmap.insert(name, pixel);
+                    kll.pixelmap.insert(index, pixel);
                 }
                 Rule::animdef => {
                     let mut parts = line.into_inner();
@@ -226,16 +436,39 @@ impl KllState<'_> {
                 Rule::animframe => {
                     let mut parts = line.into_inner();
                     let mut lhs = parts.next().unwrap().into_inner();
+                    let mut rhs = parts.next().unwrap().into_inner();
+
                     let name = lhs.next().unwrap().as_str();
                     let index = lhs.next().unwrap().as_str().parse::<usize>().unwrap();
-                    let value = parts.next().unwrap().as_str();
+
+                    let mut rparts = rhs.next().unwrap().into_inner();
+                    let pos = rparts.next().unwrap().as_str();
+                    let channels = rparts.next().unwrap();
+
+                    let mut values = vec![];
+                    for c in channels.into_inner() {
+                        let color = c.as_str();
+                        let value = match &color[0..1] {
+                            "+" | "-" => PixelColor::Relative(color.parse::<isize>().unwrap()),
+                            _ => PixelColor::Rgb(parse_int(color)),
+                        };
+                        values.push(value);
+                    }
+                    let pixel = Pixel {
+                        range: PixelRange {
+                            index: Some(PixelAddr::Absolute(parse_int(pos))),
+                            ..Default::default()
+                        },
+                        channel_values: values,
+                    };
+
                     //println!("New frame '{}' -> '{}'", name, value);
                     let animation = kll.animations.entry(name).or_default();
                     let frames = &mut animation.frames;
                     if frames.len() <= index {
-                        frames.resize(index + 1, "");
+                        frames.resize(index + 1, Pixel::default());
                     }
-                    frames[index] = value;
+                    frames[index] = pixel;
                 }
                 Rule::EOI => {}
                 _ => unreachable!(),
